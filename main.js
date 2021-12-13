de0adbcbd3b7f14c24f81f17a30e553d4ff5e368
UdonKoneko <udonkoneko55@gmail.com>
2021-12-13 15:31:18 +0900

    [add]タスクを保存する用のコード一旦コミット 未動作確認

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ const app = new Vue({
     // 使用するメソッド
   },
 });
+
+var STRAGE_KEY = "todos-vuejs-demo";
+var todoStrage = {
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STRAGE_KEY) || "[]");
+    todos.forEach(function (todo, index) {
+      todo.id = index;
+    });
+    todoStrage.uid = todos.length;
+    return todos;
+  },
+  save: function (todos) {
+    localStorage.setItem(STRAGE_KEY, JSON.stringify(todos));
+  },
+};
